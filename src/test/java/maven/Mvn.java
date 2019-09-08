2808e0b752bbe5e083e3b3edc8ca45b1cc43173b
“[mycheckin]git config --global user.name “[mycheckin] <User@User-PC>
2019-09-08 16:01:04 -0400

    Jenkins

---
+++ src/test/java/maven/Mvn.java
@@ -0,0 +1,13 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Mvn 
+{
+	@Test
+	public void sample()
+	{
+		System.out.println("Jenkins is working................");
+	}
+
+}
